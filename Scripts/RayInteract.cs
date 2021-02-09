90852070c8ac086fe64c8819cb9e69d6708b87a4
LgBattery <62005502+LgBattery@users.noreply.github.com>
2021-02-09 13:44:56 -0800

    qFIXED IHGSIHJ]

---
+++ Scripts/RayInteract.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RayInteract : MonoBehaviour
+{
+    private Transform cam;
+    public float checkDistance;
+    public KeyCode interact;
+
+    [HideInInspector]
+    public string text;
+
+    public Text textObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Physics.Raycast(cam.position, cam.forward, out hit, checkDistance);
+
+        // If it has required components and can interact
+        if (hit.transform != null && hit.transform.GetComponent<RayInteractRecieve>() && hit.transform.GetComponent<RayInteractRecieve>().canInteract)
+        {
+            // Send data to object if one has been hit and the interact button has been pressed
+            if (hit.transform != null && Input.GetKeyDown(interact))
+            {
+                hit.transform.GetComponent<RayInteractRecieve>().Interact();
+            }
+
+            // If there is a possible interaction recieve possible interaction
+            if (hit.transform != null)
+            {
+                hit.transform.GetComponent<RayInteractRecieve>().Recieve(this);
+                if (textObject)
+                    textObject.text = text;
+            }
+        }
+        else
+        {
+            if (textObject)
+                textObject.text = " ";
+        }
+    }
+}
