7b69c96bd8db56b267dff07f8b75503a1ed9dac1
Shmoog1 <78767065+Shmoog1@users.noreply.github.com>
2021-02-09 20:25:32 -0800

    Add files via upload

---
+++ GunEtc/AmmoCrate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoCrate : MonoBehaviour
+{
+   private Gun[] gun; 
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Player")
+        
+        
+        {
+            GameObject other = collision.gameObject; 
+            Destroy(gameObject);
+
+           gun = other.GetComponentsInChildren<Gun>();
+           gun[0].CurrentStockpile += 20;
+        }
+    }
+}
