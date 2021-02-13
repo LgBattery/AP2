6d7af8e08da3566f479067f16fdb60f3e6dbcecd
Shmoog1 <78767065+Shmoog1@users.noreply.github.com>
2021-02-12 19:23:19 -0800

    Add files via upload

---
+++ GunEtc/GunScripts/AmmoCrate.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoCrate : MonoBehaviour
+{
+   private Gun[] gun; 
+   public int ammoCapacity;
+   public GameObject crackedVersion;
+  
+
+
+    void Update()
+    {
+        
+    }
+    
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Collider>().tag == "Player");
+        
+        
+        {
+            Debug.Log("poggers !");
+           
+        GameObject brokenCrate = Instantiate(crackedVersion, transform.position, transform.rotation);
+        Destroy(brokenCrate, 8);
+        Destroy(gameObject);
+
+       
+           gun = other.GetComponentsInChildren<Gun>();
+           if (gun[0])
+        {
+            gun[0].CurrentStockpile += ammoCapacity;
+        }  
+        }
+    }
+}
---
+++ GunEtc/GunScripts/Target.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class Target : MonoBehaviour {
+
+
+    public float health = 50f; // Public health float
+    // Select which bool your object represents
+    public bool NPC;
+    public bool Explosive;
+    public bool MiscObject;
+
+    //EXPLOSIVE
+    [SerializeField] private GameObject CrackedVersion;
+    private float debrisLifetime = 15f;
+    private float particleLifeTime = 4f;
+    [SerializeField] private GameObject explosionEffect;
+    
+
+    public void TakeDamage (float amount) // Takes health
+    {
+        //NPC
+        
+        health -= amount; 
+        if (health <= 0f && NPC == true) // Tells the computer to Die () if health is less than or equal to 0
+        {
+            Die();
+        }
+
+        // Explosive
+
+         if (health <= 0f && Explosive == true) 
+         {
+              DieExplosive();
+         }
+
+    }
+
+    void Die ()
+    {
+        Destroy(gameObject); 
+
+        // death animation
+        // Particle effect/shader perhaps
+    }
+
+    void DieExplosive()
+    {
+        GameObject ObjectFractured = Instantiate(CrackedVersion, transform.position, transform.rotation);
+        Destroy(ObjectFractured, debrisLifetime);
+        Destroy(gameObject);
+
+        GameObject ExpClone = Instantiate(explosionEffect, transform.position, transform.rotation);
+        Destroy(ExpClone, particleLifeTime);
+    }
+
+}
